74beef4b0e392c98195e26bbe2afe38edc70025a
Lei Wei Hao <138362179+John-Leiweihao@users.noreply.github.com>
2024-04-28 23:03:30 +0800

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import streamlit as st
 st.set_page_config(page_title="PE-GPT", page_icon="💎", layout="centered",
                    initial_sidebar_state="auto", menu_items=None)
 
-st.markdown(
+st.write(
         """
         ## PE-GPT
         """
