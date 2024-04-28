79805bfcefe5b4a16c64cf362b945c357bbf18a9
Lei Wei Hao <138362179+John-Leiweihao@users.noreply.github.com>
2024-04-28 22:41:34 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
 import streamlit as st
-video_file = open('DesignCase2.mp4', 'rb')
-video_bytes = video_file.read()
+video_file1 = open('DesignCase1.mp4', 'rb')
+video_bytes1 = video_file1.read()
 
-st.video(video_bytes)
+st.video(video_bytes1)
+
+video_file2 = open('DesignCase2.mp4', 'rb')
+video_bytes2 = video_file2.read()
+
+st.video(video_bytes2)
