770eece2de3af88d446ed11e24d912842def64bc
Lei Wei Hao <138362179+John-Leiweihao@users.noreply.github.com>
2024-04-28 23:48:32 +0800

    Add files via upload

---
+++ pages/Design Case1.py
@@ -0,0 +1,8 @@
+import streamlit as st
+st.set_page_config(page_title="PE-GPT", page_icon="💎", layout="centered",
+                   initial_sidebar_state="auto", menu_items=None)
+st.title("PE-GPT🤖")
+video_file1 = open('DesignCase1.mp4', 'rb')
+video_bytes1 = video_file1.read()
+
+st.video(video_bytes1)
---
+++ pages/Design Case2.py
@@ -0,0 +1,8 @@
+import streamlit as st
+st.set_page_config(page_title="PE-GPT", page_icon="💎", layout="centered",
+                   initial_sidebar_state="auto", menu_items=None)
+st.title("PE-GPT🤖")
+video_file2 = open('DesignCase2.mp4', 'rb')
+video_bytes2 = video_file2.read()
+
+st.video(video_bytes2)
