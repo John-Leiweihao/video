a6604a7995d3faab96234c1ee0e5d5e96b52d532
Lei Wei Hao <138362179+John-Leiweihao@users.noreply.github.com>
2024-04-28 23:56:04 +0800

    Update Design Case2.py

--- pages/Design Case2.py
+++ pages/Design Case2.py
@@ -1,7 +1,7 @@
 import streamlit as st
 st.set_page_config(page_title="PE-GPT", page_icon="💎", layout="centered",
                    initial_sidebar_state="auto", menu_items=None)
-st.title("PE-GPT🤖")
+st.title("PE-GPT-Design Case2🤖")
 video_file2 = open('DesignCase2.mp4', 'rb')
 video_bytes2 = video_file2.read()
 
