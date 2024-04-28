da259aafd3a9ef5e14b7ecb7b6d0a17fc2234402
Lei Wei Hao <138362179+John-Leiweihao@users.noreply.github.com>
2024-04-28 23:02:17 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
 import streamlit as st
 st.set_page_config(page_title="PE-GPT", page_icon="💎", layout="centered",
                    initial_sidebar_state="auto", menu_items=None)
+
+    st.markdown(
+        """
+        ## PE-GPT
+        """
+    )
 st.title("PE-GPT🤖")
 import streamlit as st
 video_file1 = open('DesignCase1.mp4', 'rb')
