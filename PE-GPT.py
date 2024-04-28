a784a1fabfb2332ffdf3fb52f336fabbd24cdb18
Lei Wei Hao <138362179+John-Leiweihao@users.noreply.github.com>
2024-04-28 23:53:52 +0800

    Update PE-GPT.py

--- PE-GPT.py
+++ PE-GPT.py
@@ -7,13 +7,15 @@ st.write("# Power Electronics GPT (PE-GPT)🤖,Welcome👋")
 st.sidebar.success("Please select a demo above.")
 st.markdown(
     """
-    This repository presents demo videos of using "Power Electronics GPT (PEGPT)" for the modulation design of dual-active-bridge converters. Source codes will be open-sourced progressively.！
-    ### Design Case1(analyzed in the manuscript)
-    -1st user request: minimal current stress /n
-    -2nd user request (iterative design): extended soft switching ranges /n
-    -3rd user request: fine-tuning of PANN by uploading few time-series samples
+    This repository presents demo videos of using "Power Electronics GPT (PEGPT)" for the modulation design of dual-active-bridge converters. Source codes will be open-sourced progressively.
+
+    ### Design Case1 (analyzed in the manuscript)
+    - 1st user request: minimal current stress  
+    - 2nd user request (iterative design): extended soft switching ranges  
+    - 3rd user request: fine-tuning of PANN by uploading few time-series samples  
+
     ### Design Case 2:
-    -1st user request: minimal current stress, full-range zero voltage switching, and relatively easy implementation /n
-    -2nd user request (iterative design): enhanced current stress performance /n
+    - 1st user request: minimal current stress, full-range zero voltage switching, and relatively easy implementation  
+    - 2nd user request (iterative design): enhanced current stress performance
 """
 )
