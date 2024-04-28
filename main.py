011dd8a9be5dd368ea9266c35fb14e451e419a74
Lei Wei Hao <138362179+John-Leiweihao@users.noreply.github.com>
2024-04-28 22:59:50 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import streamlit as st
 st.set_page_config(page_title="PE-GPT", page_icon="💎", layout="centered",
                    initial_sidebar_state="auto", menu_items=None)
 st.title("PE-GPT🤖")
