fc904231dcec5bffb9165d9d10bd0276f7385310
Lei Wei Hao <138362179+John-Leiweihao@users.noreply.github.com>
2024-07-09 23:20:33 +0800

    Update PE-GPT.py

--- PE-GPT.py
+++ PE-GPT.py
@@ -5,7 +5,7 @@ st.set_page_config(page_title="PE-GPT", page_icon="💎", layout="centered",
 
 st.markdown(
     """
-    <h2 style='font-size: 40px; color: yellow;'>PE-GPT🤖 : A New Paradigm for Power Electronics Design, Welcome!👋</h2>
+    <h2 style='font-size: 40px; color: red;'>PE-GPT🤖 : A New Paradigm for Power Electronics Design, Welcome!👋</h2>
     """,
     unsafe_allow_html=True
 )
